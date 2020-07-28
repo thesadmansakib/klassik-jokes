96484ac8df4b6a90e7c4b959fa7ee5d40cbe721f
Sadman Sakib <sadman4523@gmail.com>
2020-07-28 20:56:28 +0600

    Updates

---
+++ precache-manifest.5c0cdc4ea4721f62422b83ce1ec61c7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372131f9da9271b2f7ccd04b03cc65cf",
+    "url": "/klassik-jokes/index.html"
+  },
+  {
+    "revision": "792caf7cb8d944057a2d",
+    "url": "/klassik-jokes/static/css/main.ca3ae6bd.chunk.css"
+  },
+  {
+    "revision": "0fad8f13fc1aa24b49ae",
+    "url": "/klassik-jokes/static/js/2.4a5d455b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/klassik-jokes/static/js/2.4a5d455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792caf7cb8d944057a2d",
+    "url": "/klassik-jokes/static/js/main.c4d03cd4.chunk.js"
+  },
+  {
+    "revision": "384529e4da831ba4942a",
+    "url": "/klassik-jokes/static/js/runtime-main.f4527f73.js"
+  },
+  {
+    "revision": "6fcc7f3b50a1940be07de185a282e54d",
+    "url": "/klassik-jokes/static/media/logo-smiley.6fcc7f3b.png"
+  },
+  {
+    "revision": "9c1e9b24bf98ceec42fd7af706b5addb",
+    "url": "/klassik-jokes/static/media/logo.9c1e9b24.png"
+  }
+]);
