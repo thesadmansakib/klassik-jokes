0f426b5e74820b2ef5ba614d4a1515ec09b85481
Sadman Sakib <sadman4523@gmail.com>
2020-07-29 00:03:18 +0600

    Home Page Completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import random from "../images/random-joke.png";
+import DadJoke from "../images/dad-joke.png";
+import style from "./Home.module.css";
 
 const Home = () => {
     return (
-        <div>
-            <p>Home</p>
+        <div className={style.container}>
+            <Link to="/random">
+                <button className={style.btn}>
+                    <img className={style.icon} src={random} alt="Random" />
+                    Random Jokes
+                </button>
+            </Link>
+            <Link to="/daddy">
+                <button className={style.btn}>
+                    Dad Jokes
+                    <img className={style.icon} src={DadJoke} alt="Random" />
+                </button>
+            </Link>
         </div>
     );
 };
